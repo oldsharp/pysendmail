d685c00baf11efb17865cdbe3aa1c5a1cc245a69
Ray Chen <oldsharp@gmail.com>
2014-07-11 11:04:38 +0800

    Refine script

--- sendmail.py
+++ sendmail.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import os.path
 import smtplib
 
@@ -12,17 +14,24 @@ from email.utils import formatdate
 COMMASPACE = ', '
 
 SMTP_HOST = '10.177.9.96'
-SMTP_USER = 'raychen'
-SMTP_PASS = 'xxxxxx'
+SMTP_USER = 'username'
+SMTP_PASS = 'password'
 
-FROM = 'Rui.Chen@sandisk.com'
-TO = ['oldsharp@163.com', 'oldsharp@gmail.com']
-# TODO: add cc/bcc line ?
 DATE = formatdate(localtime=True)
 
+FROM = 'Rui.Chen@sandisk.com'
+
+TO = ['oldsharp@163.com', 'oldsharp@gmail.com',]
+
+CC = ['cc1@example.com', 'cc2@example.com',]
+
+BCC = ['bcc1@example.com', 'bcc2@example.com',]
+
 SUBJECT = 'subject'
+
 BODY = '/home/ray/sendmail/mailbody'
-ATTACH_FILES = ['/home/ray/sendmail/attach_sample', '/home/ray/sendmail/sendmail.py']
+
+ATTACH_FILES = ['/path/to/attach1', '/path/to/attach2']
 
 
 def sendmail():
@@ -30,34 +39,34 @@ def sendmail():
     msg = MIMEMultipart()
 
     msg['From'] = FROM
-    msg['To'] = COMMASPACE.join(TO)
-    msg['Date'] = formatdate(localtime=True)
+
+    if TO:
+        msg['To'] = COMMASPACE.join(TO)
+    if CC:
+        msg['Cc'] = COMMASPACE.join(CC)
+    if BCC:
+        msg['Bcc'] = COMMASPACE.join(BCC)
+
+    msg['Date'] = DATE
     msg['Subject'] = SUBJECT
 
-    # Open a plain text file for reading.  For this example, assume that
-    # the text file contains only ASCII characters.
     fp = open(BODY, 'rb')
-    # Add a text/plain message
     msg.attach(MIMEText(fp.read()))
     fp.close()
 
-    msg.attach(MIMEText(BODY))
-
     for f in ATTACH_FILES:
-        part = MIMEBase('application', 'octet-stream')
+        attach_file = MIMEBase('application', 'octet-stream')
         fp = open(f, 'rb')
-        part.set_payload(fp.read())
+        attach_file.set_payload(fp.read())
         fp.close()
-        encoders.encode_base64(part)
-        part.add_header('Content-Disposition', 'attachment',
-                        filename=os.path.basename(f))
-        msg.attach(part)
+        encoders.encode_base64(attach_file)
+        attach_file.add_header('Content-Disposition', 'attachment',
+                               filename=os.path.basename(f))
+        msg.attach(attach_file)
 
-    # Send the message via our own SMTP server, but don't include the
-    # envelope header.
     s = smtplib.SMTP(host=SMTP_HOST)
-    #s.login(user='raychen', password='xxx')
-    s.sendmail(FROM, TO, msg.as_string())
+    #s.login(user=SMTP_USER, password=SMTP_PASS)
+    s.sendmail(FROM, TO+CC+BCC, msg.as_string())
     s.quit()
 
 
